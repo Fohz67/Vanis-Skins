36ab0503563f4c2b53a70732fe158e7f8274a1f9
Fohz <114908868+Fohz67@users.noreply.github.com>
2024-03-19 03:30:02 +0100

    Create extractSkinsId.js

---
+++ extractSkinsId.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+let urls = [];
+try {
+    const data = fs.readFileSync(filePath, 'utf8');
+
+  urls = JSON.parse(data);
+} catch (error) {
+    console.error('Error reading or parsing the file:', error);
+    return;
+}
+
+const filteredUrls = urls
+  .filter(url => url.startsWith("https://skins.vanis.io/s/"))
+  .map(url => {
+    const key = url.replace(/^https:\/\/skins\.vanis\.io\/s\//, '');
+    const cleanedKey = key.replace(/[^a-zA-Z0-9]/g, '');
+    return `"${cleanedKey}": "${url}"`;
+  });
+
+const jsonString = `{ ${filteredUrls.join(', ')} }`;
+
+fs.writeFile('extractedIds.json', jsonString, 'utf8', (err) => {
+  if (err) {
+    console.error('Error writing to file:', err);
+  } else {
+    console.log('The file output2.json has been successfully created.');
+  }
+});
