aefc4103502219aefffe177b9f9ff42052123d25
Fohz <114908868+Fohz67@users.noreply.github.com>
2024-03-19 03:26:42 +0100

    Create parseSkins.js

---
+++ parseSkins.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const filePath = process.argv[2];
+
+let jsonData = {};
+try {
+    const data = fs.readFileSync(filePath, 'utf8');
+
+    jsonData = JSON.parse(data);
+} catch (error) {
+    console.error('Error reading or parsing the file:', error);
+    process.exit(84);
+}
+
+const allSkins = [];
+
+for (const key in jsonData) {
+    const subObj = jsonData[key];
+
+    for (const subKey in subObj) {
+        const skinsValue = subObj[subKey].skins;
+
+        if (skinsValue && typeof skinsValue === 'string') {
+            try {
+                const skinsArray = JSON.parse(skinsValue);
+
+                allSkins.push(...skinsArray.filter(skin => skin.trim() !== ''));
+            } catch (error) {
+                console.error(`Error while parsing JSON string for key ${key}/${subKey}`);
+            }
+        }
+    }
+}
+
+const outputJson = JSON.stringify(allSkins, null, 2);
+
+fs.writeFile('skinsParsed.json', outputJson, 'utf8', (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+    } else {
+        console.log('Output has been written to output.json');
+    }
+});
